c57d18dfd147b713406e430d04e9e7faa0b57876
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-30 15:28:27 +0300

    Authentication by phone/password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:phone, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: [:phone]
+  end
 end
