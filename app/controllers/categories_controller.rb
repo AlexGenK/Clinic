e1b26023591e1fe01bb9c0cc5e12b083d509705d
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-30 18:59:31 +0300

    Categories can be added and destroyed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,30 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_category, only: [:destroy]
+
   def index
+    @categories = Category.order(:name)
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    flash[:alert] = 'Unable to add category' unless @category.save
+    redirect_to categories_path
+  end
+
+  def destroy
+    flash[:alert] = 'Unable to delete category' unless @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
