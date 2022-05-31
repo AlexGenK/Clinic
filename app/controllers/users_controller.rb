c80b86f1abb6cfd9c94192e026c099b22cdd3b99
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-31 13:50:30 +0300

    Added users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:destroy, :edit, :update]
+
+  def index
+    @users = User.order(:username)
+  end
+
+  def edit
+    @cat_list = Category.all.collect {|cat| [cat.name, cat.id]}
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      flash[:alert] = 'Unable to edit user'
+      render :edit
+    end
+  end
+
+  def destroy
+    flash[:alert] = 'Unable to delete user' unless @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :phone, :user_role, :doctor_role, :admin_role, :category_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :categories
 
+  resources :users
+
   root to: 'categories#index'
 end
