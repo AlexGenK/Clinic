c153c17899e96597eb71b11d654ce6024c3bc3c1
Alexey Kozhyn <suburbakskin@gmail.com>
2022-06-01 16:48:24 +0300

    Remove of unused functionality for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:destroy, :edit, :update]
+  before_action :set_user, only: [:edit, :update]
   load_and_authorize_resource
 
   def index
@@ -16,13 +16,9 @@ class UsersController < ApplicationController
       redirect_to users_path
     else
       flash[:alert] = 'Unable to edit user'
+      @cat_list = Category.all.collect {|cat| [cat.name, cat.id]}
       render :edit
     end
-  end
-
-  def destroy
-    flash[:alert] = 'Unable to delete user' unless @user.destroy
-    redirect_to users_path
   end
 
   private
