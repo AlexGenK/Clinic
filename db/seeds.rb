f45061f826c14cf43e7c048933a2b25fa3817049
Alexey Kozhyn <suburbakskin@gmail.com>
2022-06-01 16:53:24 +0300

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: 'Admin', email: 'admin@mail.no', phone: '+380503332233', password: '123456', admin_role: true, user_role: false, doctor_role: false)
