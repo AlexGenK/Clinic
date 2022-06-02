6d2a4e56c7f74d2b3b4a402ef12ac7ad494a5ae0
Alexey Kozhyn <suburbakskin@gmail.com>
2022-06-02 15:10:53 +0300

    Role must be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   validates :phone, uniqueness: true
   validates :phone, format: { with: /\A\+380[1,3-9][0-9]{8}\z/, message: 'Phone number must be in format +380yyxxxxxxx' }
 
+  before_validation :reset_roles
+
   def email_required?
     false
   end
@@ -19,5 +21,10 @@ class User < ApplicationRecord
   
   def will_save_change_to_email?
     false
+  end
+
+  def reset_roles
+    self.user_role, self.doctor_role = false, false if self.admin_role?
+    self.user_role, self.admin_role = false, false if self.doctor_role?
   end
 end
